1e7eab2a67419ee1f5d36f6843efc6d7230582e4
Han Xu <han.xu@nxp.com>
2021-05-26 11:17:01 -0500

    LF-1723: spi: lpspi: run perclk_rate sanity check
    Coverity reports Division or modulo by zero potential issue(CWE-369)
    about the perclk_rate calculation. Run sanity check first to avoid this
    problem.
    
    Signed-off-by: Han Xu <han.xu@nxp.com>
    Reviewed-by: Frank Li <frank.li@nxp.com>
    (cherry picked from commit 3ea61870ff252e1931efd131b2b42cf33a39c764)

--- drivers/spi/spi-fsl-lpspi.c
+++ drivers/spi/spi-fsl-lpspi.c
@@ -320,6 +320,11 @@ static int fsl_lpspi_set_bitrate(struct fsl_lpspi_data *fsl_lpspi)
 	u8 prescale;
 
 	perclk_rate = clk_get_rate(fsl_lpspi->clk_per);
+	if (!perclk_rate) {
+		dev_err(fsl_lpspi->dev, "per-clk rate was not set\n");
+		return -EINVAL;
+	}
+
 	prescale_max = fsl_lpspi->devtype_data->prescale_max;
 
 	if (!config.speed_hz) {
