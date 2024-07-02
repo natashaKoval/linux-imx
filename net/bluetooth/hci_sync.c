9b125a48d39c153e3be43a4cb27f9aed65db2cd2
Natalia Kovalenko <natasha.k@variscite.com>
2024-07-02 16:31:01 +0200

    Bluetooth: hci_sync: Fix missing assignment to hdev->req_skb
    Assign to hdev->req_skb the pointer to buffer in hci_cmd_sync_complete
    function, otherwise __hci_cmd_sync will return -ENODATA.
    
    derived from upstream commit cba6b758711cab946c787f7c15be92cc749b8e1f
    
    Signed-off-by: Natalia Kovalenko <natasha.k@variscite.com>

--- net/bluetooth/hci_sync.c
+++ net/bluetooth/hci_sync.c
@@ -23,6 +23,9 @@ static void hci_cmd_sync_complete(struct hci_dev *hdev, u8 result, u16 opcode,
 	hdev->req_result = result;
 	hdev->req_status = HCI_REQ_DONE;
 
+	if (skb)
+                hdev->req_skb = skb_get(skb);
+
 	wake_up_interruptible(&hdev->req_wait_q);
 }
 
